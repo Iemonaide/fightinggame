8667192a372e5b96b616c6fa43b84ce6712c2b2d
SodaPop <147830198+SofaPop@users.noreply.github.com>
2025-09-11 19:26:22 -0400

    uhhh something probably

--- fightinggame/scripts/characterselectscreen.gd
+++ fightinggame/scripts/characterselectscreen.gd
@@ -1,4 +1,4 @@
 extends Control
 
-func _on_returnBt_pressed() -> void:
+func _on_backtostartbt_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/startmenu.tscn")
--- fightinggame/scripts/startmenu.gd
+++ fightinggame/scripts/startmenu.gd
@@ -1,14 +1,11 @@
 extends Control
 
-func _ready():
-	$VBoxContainer/startBt.grab_focus()
-
 func _on_startbt_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_charselectbt_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/characterselectscreen.tscn")	
 
 
 func _on_quitbt_pressed() -> void:
