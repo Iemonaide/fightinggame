095942bc8cf3c4d36c652dc58e9cc654a91a9dea
Iemonaide <147830442+Iemonaide@users.noreply.github.com>
2025-09-25 09:36:40 -0400

    multiplayer update

--- fightinggame/scripts/startmenu.gd
+++ fightinggame/scripts/startmenu.gd
@@ -10,3 +10,13 @@ func _on_charselectbt_pressed() -> void:
 
 func _on_quitbt_pressed() -> void:
 	get_tree().quit()
+
+func become_host():
+	print("Become host pressed")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.become_host()
+
+func join_as_player_2():
+	print("Join as PLayer 2")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.join_as_player_2()
